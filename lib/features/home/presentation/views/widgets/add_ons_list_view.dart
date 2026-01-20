62e304779989cce2648f75bf40041c85a3998b6b
khalidadel <khalidadel209@gmail.com>
2026-01-20 04:16:10 +0200

    add_ons_list_view done

--- lib/features/home/presentation/views/widgets/add_ons_list_view.dart
+++ lib/features/home/presentation/views/widgets/add_ons_list_view.dart
@@ -2,20 +2,39 @@
 import 'package:flutter/material.dart';
 import 'package:homemade_food_app/features/home/presentation/views/widgets/custom_addon_option_button.dart';
 
-class AddOnsListView extends StatelessWidget {
+class AddOnsListView extends StatefulWidget {
   const AddOnsListView({
     super.key,
   });
 
   @override
+  State<AddOnsListView> createState() => _AddOnsListViewState();
+}
+
+class _AddOnsListViewState extends State<AddOnsListView> {
+  List<int> selectedIndexes = [];
+  @override
   Widget build(BuildContext context) {
     return ListView.separated(
         shrinkWrap: true,
         physics: NeverScrollableScrollPhysics(),
-        itemBuilder: (index, context) => CustomAddOnOptionButton(),
+        itemBuilder: (context, index) {
+          return CustomAddOnOptionButton(
+            isSelected: selectedIndexes.contains(index),
+            onTap: (){
+              setState(() {
+                if(selectedIndexes.contains(index)){
+                  selectedIndexes.remove(index);
+                }else{
+                  selectedIndexes.add(index);
+                }
+              });
+            },
+          );
+        },
         separatorBuilder: (index, context) => const SizedBox(
           height: 10,
         ),
-        itemCount: 3);
+        itemCount: 5);
   }
 }
--- lib/features/home/presentation/views/widgets/custom_addon_option_button.dart
+++ lib/features/home/presentation/views/widgets/custom_addon_option_button.dart
@@ -6,51 +6,54 @@ import '../../../../../core/utilities/styles.dart';
 class CustomAddOnOptionButton extends StatelessWidget {
   const CustomAddOnOptionButton({
     super.key,
+    required this.onTap,
+    required this.isSelected,
   });
+
+  final VoidCallback onTap;
+  final bool isSelected;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 58,
-      padding: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        shape: BoxShape.rectangle,
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(15),
-        border: Border.all(
-          color: Colors.grey.shade200,
-          width: 1.5,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 58,
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          color: isSelected ? kSecondaryColor : Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(
+            color: Colors.grey.shade200,
+            width: 1.5,
+          ),
         ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            padding: EdgeInsets.all(6),
-            height: 21,
-            width: 21,
-            decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(3),
-                border:Border.all(width: 1,color: Colors.grey.shade300)
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(6),
+              height: 21,
+              width: 21,
+              decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  color: isSelected ? kPrimaryColor : Colors.white,
+                  borderRadius: BorderRadius.circular(6),
+                  border: Border.all(width: 1, color: Colors.grey.shade300)),
             ),
-
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          Text(
-              'Extra Cheese',
-              style: Styles.textStyle14
-          ),
-          const Spacer(),
-          Text(
-            '+150 EGY',
-            style: Styles.textStyle14.copyWith(color: kPrimaryColor),
-          )
-        ],
+            const SizedBox(
+              width: 10,
+            ),
+            Text('Extra Cheese', style: Styles.textStyle14),
+            const Spacer(),
+            Text(
+              '+150 EGY',
+              style: Styles.textStyle14.copyWith(color: kPrimaryColor),
+            )
+          ],
+        ),
       ),
     );
   }
