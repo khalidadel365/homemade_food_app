0891b2cc2c02da7201b2e1da6706360429a00120
khalidadel <khalidadel209@gmail.com>
2026-02-02 02:25:29 +0200

    add cache helper file

--- lib/features/profile/presentation/views/widgets/custom_profile_image.dart
+++ lib/features/profile/presentation/views/widgets/custom_profile_image.dart
@@ -22,7 +22,9 @@ class CustomProfileImage extends StatelessWidget {
               ),
               child: CachedNetworkImage(
                   fit: BoxFit.cover,
-                  placeholder: (context, url) => const CircularProgressIndicator(),
+                  placeholder: (context, url) => const CircularProgressIndicator(
+                    color: kPrimaryColor,
+                  ),
                   errorWidget: (context, url, error) => const Icon(Icons.error),
                   imageUrl: 'https://cdn.psychologytoday.com/sites/default/files/styles/article-inline-half-caption/public/field_blog_entry_images/2018-09/shutterstock_648907024.jpg?itok=0hb44OrI')
           ),
