4ab24a1704310c15134e96ac7167a47d2dd860fe
Khalid Adel <khalidadel209@gmail.com>
2026-04-24 21:36:24 +0300

    update url

--- lib/core/utilities/api_service.dart
+++ lib/core/utilities/api_service.dart
@@ -7,7 +7,7 @@ class ApiService {
     dio = Dio(
       BaseOptions(
         //baseUrl: 'https://unsegregated-itchingly-charisse.ngrok-free.dev',
-        baseUrl: 'http://10.0.2.2:8000',
+        baseUrl: 'https://homemadefood.onrender.com',
         connectTimeout: const Duration(seconds: 10),
         receiveTimeout: const Duration(seconds: 10),
         receiveDataWhenStatusError: true,
